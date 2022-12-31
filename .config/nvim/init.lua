f0d694e57f7ca057035a61df4680251ecc6e04a9
SebMD <sebmd@protonmail.ch>
2022-12-31 12:45:01 +0100

    2022-12-31-12:44:59

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,14 +2,14 @@
 require("custom.pluginy")
 -- Ustawienia
 require("custom.config") -- ~/.config/nvim/lua/custom/config.lua
-require("custom.keys") -- ~/.config/nvim/lua/custom/keys.lua
+require("custom.skroty") -- ~/.config/nvim/lua/custom/skroty.lua
 require("custom.funkcje") -- ~/.config/nvim/lua/custom/funkcje.lua
--- Pluginy
-require("custom.lualine") -- ~/.config/nvim/lua/custom/lualine.lua
-require("custom.leap") -- ~/.config/nvim/lua/custom/leap.lua
-require("custom.gitsigns") -- ~/.config/nvim/lua/custom/gitsigns.lua
-require("custom.nvim-tree") -- ~/.config/nvim/lua/custom/nvim-tree.lua
-require("custom.kommentary") -- ~/.config/nvim/lua/custom/kommentary.lua
-require("custom.pears") -- ~/.config/nvim/lua/custom/pears.lua
-require("custom.zenmode") -- ~/.config/nvim/lua/custom/zenmode.lua
 require("custom.komendy") -- ~/.config/nvim/lua/custom/komendy.lua
+-- Pluginy
+require("custom.pluginy.lualine") -- ~/.config/nvim/lua/custom/pluginy/lualine.lua
+require("custom.pluginy.leap") -- ~/.config/nvim/lua/custom/pluginy/leap.lua
+require("custom.pluginy.gitsigns") -- ~/.config/nvim/lua/custom/pluginy/gitsigns.lua
+require("custom.pluginy.nvim-tree") -- ~/.config/nvim/lua/custom/pluginy/nvim-tree.lua
+require("custom.pluginy.kommentary") -- ~/.config/nvim/lua/custom/pluginy/kommentary.lua
+require("custom.pluginy.pears") -- ~/.config/nvim/lua/custom/pluginy/pears.lua
+require("custom.pluginy.zenmode") -- ~/.config/nvim/lua/custom/pluginy/zenmode.lua
